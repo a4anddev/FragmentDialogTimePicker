991d42ca0a124cca05adecfc3dc982bfb595baca
a4anddev <a4anddev@gmail.com>
2018-06-27 23:22:11 +0530

    pop up / aleart / dialoge / builder

--- app/src/main/java/c/verbswithexample/fragmentdialogtimepicker/MainActivity.java
+++ app/src/main/java/c/verbswithexample/fragmentdialogtimepicker/MainActivity.java
@@ -1,5 +1,7 @@
 package c.verbswithexample.fragmentdialogtimepicker;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +10,45 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnShow;
+    Button btnShow, dialogBuilder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnShow = findViewById(R.id.btnShow);
+        dialogBuilder = findViewById(R.id.dialogBuilder);
+
+
+
+        dialogBuilder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+
+                alert.setMessage("are you sure you want to delete")
+                .setTitle("Confirmation")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                Toast.makeText(MainActivity.this, "Removed", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "Canceled", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+
+
+
+            }
+        });
+
 
         btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
